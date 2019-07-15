f110768a450718f49b4f7cade8411009a0ba5196
nanakiksc <polcusco@gmail.com>
2019-07-15 11:36:32 +0200

    Added alpha param.

--- gg_colors.R
+++ gg_colors.R
@@ -1,4 +1,4 @@
-gg_colors <- function(n) {
+gg_colors <- function(n, alpha = 1) {
    hues = seq(15, 375, length = n + 1)
-   hcl(h = hues, l = 65, c = 100)[1:n]
+   hcl(h = hues, l = 65, c = 100, alpha = alpha)[1:n]
 }
